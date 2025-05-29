a39ba13e49994c413b92cfcf351b1dd8346cb074
Jayesh Joshi <divyeshjoshi7474@gmail.com>
2025-05-29 14:09:55 +0530

    data

---
+++ src/data/locationData.js
@@ -0,0 +1,60 @@
+export const countries = [
+  {
+    code: "IN",
+    name: "India",
+    cities: [
+      "Mumbai",
+      "Delhi",
+      "Bangalore",
+      "Chennai",
+      "Kolkata",
+      "Hyderabad",
+      "Pune",
+      "Ahmedabad",
+    ],
+  },
+  {
+    code: "US",
+    name: "United States",
+    cities: [
+      "New York",
+      "Los Angeles",
+      "Chicago",
+      "Houston",
+      "Phoenix",
+      "Philadelphia",
+    ],
+  },
+  {
+    code: "UK",
+    name: "United Kingdom",
+    cities: [
+      "London",
+      "Manchester",
+      "Birmingham",
+      "Liverpool",
+      "Leeds",
+      "Sheffield",
+    ],
+  },
+  {
+    code: "CA",
+    name: "Canada",
+    cities: [
+      "Toronto",
+      "Vancouver",
+      "Montreal",
+      "Calgary",
+      "Ottawa",
+      "Edmonton",
+    ],
+  },
+];
+
+export const countryCodes = [
+  { code: "+91", country: "India" },
+  { code: "+1", country: "US/Canada" },
+  { code: "+44", country: "UK" },
+  { code: "+33", country: "France" },
+  { code: "+49", country: "Germany" },
+];
